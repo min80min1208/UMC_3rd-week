292d756cb7425be38d40efb139321fc2f9d4ebb3
MinkyungYu <min80min1208@yonsei.ac.kr>
2024-05-10 20:37:01 +0900

    Initial Commit

---
+++ UMC_3rd weekUITests/UMC_3rd_weekUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  UMC_3rd_weekUITestsLaunchTests.swift
+//  UMC_3rd weekUITests
+//
+//  Created by 유민경 on 5/10/24.
+//
+
+import XCTest
+
+final class UMC_3rd_weekUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
